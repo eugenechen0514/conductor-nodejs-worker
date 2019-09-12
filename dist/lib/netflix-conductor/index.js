ea5ecd9ab5ba30bfebf74adbf7c496b358924b34
EugeneChen <yujiechen0514@gmail.com>
2019-09-12 15:19:16 +0800

    add dist folder

---
+++ dist/lib/netflix-conductor/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TaskState;
+(function (TaskState) {
+    TaskState["scheduled"] = "scheduled";
+    TaskState["inProgress"] = "IN_PROGRESS";
+    TaskState["failed"] = "FAILED";
+    TaskState["completed"] = "COMPLETED";
+    TaskState["cancelled"] = "CANCELLED";
+})(TaskState = exports.TaskState || (exports.TaskState = {}));
+//# sourceMappingURL=index.js.map
