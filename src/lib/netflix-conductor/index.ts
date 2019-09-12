ef98037582e955cd6492d2c377d2ba90c5114594
EugeneChen <yujiechen0514@gmail.com>
2019-09-12 16:10:16 +0800

    add more types

--- src/lib/netflix-conductor/index.ts
+++ src/lib/netflix-conductor/index.ts
@@ -1,9 +1,63 @@
+export interface TaskDefinition {
+    present: boolean;
+}
+
+export interface TaskDefinition {
+    "createTime": number,
+    "name": string,
+    "retryCount": number,
+    "timeoutSeconds": number,
+    "inputKeys"?: string[],
+    "timeoutPolicy": string, // ex: "TIME_OUT_WF"
+    "retryLogic": string, // ex: "FIXED"
+    "retryDelaySeconds": number,
+    "responseTimeoutSeconds": number,
+    "rateLimitPerFrequency": number,
+    "rateLimitFrequencyInSeconds": number
+}
+
+export interface WorkflowTask {
+    name: string,
+    taskReferenceName: string,
+    inputParameters?: any,
+    type: string, // ex: SIMPLE,
+    startDelay: number,
+    optional: boolean,
+    taskDefinition: TaskDefinition,
+    asyncComplete: boolean,
+}
+
 export interface PollTask {
-    inputData?: {
-        input: any;
-    };
-    workflowInstanceId: string;
-    taskId: string;
+    taskType: string;
+    status: TaskState;
+    inputData?: any;
+    referenceTaskName: string;
+    retryCount: number,
+    seq: number,
+    correlationId: string,
+    pollCount: number,
+    taskDefName: string,
+    scheduledTime: number,
+    startTime: number,
+    endTime: number,
+    updateTime: number,
+    startDelayInSeconds: number,
+    retried: boolean,
+    executed: boolean,
+    callbackFromWorker: boolean,
+    responseTimeoutSeconds: number,
+    workflowInstanceId: string,
+    workflowType: string,
+    taskId: string,
+    callbackAfterSeconds: number,
+    workerId: string,
+    workflowTask: WorkflowTask,
+    rateLimitPerFrequency: number,
+    rateLimitFrequencyInSeconds: number,
+    workflowPriority: number,
+    taskDefinition: TaskDefinition,
+    queueWaitTime: number,
+    taskStatus: TaskState,
 }
 
 export enum TaskState {
