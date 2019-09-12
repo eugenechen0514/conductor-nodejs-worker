ff5ded6434ce492ac43fb34dec6d23d7a185bebd
EugeneChen <yujiechen0514@gmail.com>
2019-09-12 12:04:46 +0800

    add ;

--- example/example.js
+++ example/example.js
@@ -4,23 +4,23 @@ const worker = new ConductorWorker({
     url: 'http://localhost:8080', // host
     apiPath: '/api', // base path
     workerid: 'node-worker',
-})
+});
 
-const taskType = 'test'
+const taskType = 'test';
 
 const fn = (input) => {
     return new Promise((resolve, reject) => {
       const handler = setTimeout(()=>{
-        clearTimeout(handler)
+        clearTimeout(handler);
         resolve({
             result: false,
         })
       }, 3000)
     })
-  }
+  };
 
-worker.start(taskType, fn)
+worker.start(taskType, fn);
 
 setTimeout(()=>{
   worker.stop()
-}, 10000)
+}, 10000);
